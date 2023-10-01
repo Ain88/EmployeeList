1b1da9d93a7e9941c1e4ea9d65f1aefbffbb2df0
Ain88 <ahyoung.in@hotmail.com>
2023-09-30 22:48:53 -0700

    Added employee controller/view/model

---
+++ EmployeeList/Data/EmployeeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeList.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeList.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- EmployeeList/Program.cs
+++ EmployeeList/Program.cs
@@ -1,7 +1,12 @@
+using EmployeeList.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<EmployeeDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeListConnectionString")));
 
 var app = builder.Build();
 
